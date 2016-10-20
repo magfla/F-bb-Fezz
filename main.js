f65e65b1725aa26effb750574181db685256636e
Peter Rasmussen Lubiana <peterlubiana@gmail.com>
2016-10-20 11:18:39 +0200

    Added a css stylesheet, some content to the index and some simple js, hash based navigation!

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
+var RANDOM_BACKGROUND_COLORS = false;
+
 
 $(document).ready(function(){
 	var text = "<p> Velkommmmmenenenenenenen </p>";
-
-	setInterval(function(){
-		$("*").css({background:getRandomColor()});
-	},1000);
+	app.setup();
+	
 });
 
 function getRandomColor() {
@@ -15,4 +15,36 @@ function getRandomColor() {
 		color += letters[Math.floor(Math.random() * 16)];
 	}
 	return color;
+}
+
+
+
+
+var app = {};
+
+app.setup = function(){
+	if(RANDOM_BACKGROUND_COLORS){
+		setInterval(function(){
+			$("*").css({background:getRandomColor()});
+		},1000);
+	}
+
+	$("#main-navigation a").on("click",function(e){
+		app.navItemClicked(window.location.hash);
+
+	});
+}
+
+app.updateCurrentPageTitle = function(arg){
+	$("#display-area h1").html(arg);
+}
+
+app.updateCurrentPageContent = function(arg){
+	$("#display-area p").html(arg);
+}
+
+
+app.navItemClicked = function(hash){
+	app.updateCurrentPageTitle(hash);
+	app.updateCurrentPageContent("Sadly there is not so much information about " + hash +" at the monent. Maybe you would like to help fix that?");
 }
